a9ed87dab98b5796a00b4139f0801d51a7474f9f
Husnia Sidiqi <hkpayenda@gmail.com>
2023-05-30 21:07:01 -0400

    update primery account

--- src/test/java/tek/api/model/PrimaryAccount.java
+++ src/test/java/tek/api/model/PrimaryAccount.java
@@ -1,23 +1,24 @@
 package tek.api.model;
 
+import java.util.Date;
+
 public class PrimaryAccount {
-	private Long id;
+	private long id; 
 	private String email;
-	private String firstName;
-	private String lastName;
-	private String title;
-	private String gender;
-	private String maritalStatus;
-	private String employmentStatus;
-	private String dateOfBirth;
+	private String firstName; 
+	private String lastName; 
+	private String title; 
+	private String gender; 
+	private String maritalStatus; 
+	private String employmentStatus; 
+	private Date dateOfBirth; 
+	private Object user; 
 	
 	//NoArgsConstructor
 	public PrimaryAccount() {}
 	//AllArgsConstructor
-	
-	public PrimaryAccount(Long id, String email, String firstName, String lastName, String title, String gender,
-			String maritalStatus, String employmentStatus, String dateOfBirth) {
-		super();
+	public PrimaryAccount(long id, String email, String firstName, String lastName, String title, String gender,
+			String maritalStatus, String employmentStatus, Date dateOfBirth, Object user) {
 		this.id = id;
 		this.email = email;
 		this.firstName = firstName;
@@ -27,11 +28,12 @@ public class PrimaryAccount {
 		this.maritalStatus = maritalStatus;
 		this.employmentStatus = employmentStatus;
 		this.dateOfBirth = dateOfBirth;
+		this.user = user;
 	}
+	
 	//Constructor without ID
 	public PrimaryAccount(String email, String firstName, String lastName, String title, String gender,
-			String maritalStatus, String employmentStatus, String dateOfBirth) {
-		super();
+			String maritalStatus, String employmentStatus, Date dateOfBirth) {
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -41,78 +43,66 @@ public class PrimaryAccount {
 		this.employmentStatus = employmentStatus;
 		this.dateOfBirth = dateOfBirth;
 	}
-
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
-
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public String getMaritalStatus() {
 		return maritalStatus;
 	}
-
 	public void setMaritalStatus(String maritalStatus) {
 		this.maritalStatus = maritalStatus;
 	}
-
 	public String getEmploymentStatus() {
 		return employmentStatus;
 	}
-
 	public void setEmploymentStatus(String employmentStatus) {
 		this.employmentStatus = employmentStatus;
 	}
-
-	public String getDateOfBirth() {
+	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
-
-	public void setDateOfBirth(String dateOfBirth) {
+	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+	public Object getUser() {
+		return user;
+	}
+	public void setUser(Object user) {
+		this.user = user;
+	}
 	
-
+	
 }
