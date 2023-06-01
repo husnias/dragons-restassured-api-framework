5153bd9cbc3fc89768550a5216d22f272a86e80d
Husnia Sidiqi <hkpayenda@gmail.com>
2023-06-01 18:00:52 -0400

    create new test class,and update pom file

---
+++ src/test/java/tek/api/sqa/tests/GetAllAcountTest.java
@@ -0,0 +1,23 @@
+package tek.api.sqa.tests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import tek.api.sqa.base.APITestConfig;
+import tek.api.utility.EndPoints;
+public class GetAllAcountTest extends APITestConfig {
+
+	@Test
+	public void getAllAccountTest() {
+		// Prepare the request.
+		String token = getValidToken();
+		RequestSpecification request = RestAssured.given();
+		request.header("Authorization", "Bearer " + token);
+		Response response = request.when().get(EndPoints.GET_ALL_ACCOUNTS.getValue());
+
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+
+}
--- src/test/java/tek/api/utility/DataGenerator.java
+++ src/test/java/tek/api/utility/DataGenerator.java
@@ -1,4 +1,50 @@
 package tek.api.utility;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.github.javafaker.Faker;
 
 public class DataGenerator {
+	
+private Faker faker; 
+	
+	public DataGenerator() {
+		this.faker = new Faker();
+	}
+	
+	public String getFirstName() {
+		return faker.name().firstName(); 
+	}
+	
+	public String getLastName() {
+		return faker.name().lastName(); 
+	}
+	
+	public String getEmail() {
+		String firstName = getFirstName(); 
+		String lastName = getLastName(); 
+		return firstName + "_" + lastName + "@gmail.com";
+	}
+	
+	public String getEmail(String firstName, String lastName, String provider) {
+		return firstName + "_" + lastName + "@" + provider; 
+	}
+	
+	public String getJobTitle() {
+		return faker.job().position(); 
+	}
+	
+	public Date getDateOfBirth() {
+		Date date = faker.date().birthday();
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String formattedDate = formatter.format(date);
+		try {
+			return formatter.parse(formattedDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("Date Parse Exception ");
+		}
+	}
 }
+
